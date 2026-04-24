c773f95e6f56f1aa5e1d8794bd3d9026d29276b7
alexandru.buca <alexandru.buca@stud.acs.ubp.ro>
2026-04-25 02:22:07 +0300

    Added logic for first level

---
+++ Assets/_Scripts/DancingStatue.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class DancingStatue : MonoBehaviour
+{
+    public static DancingStatue instance;
+    Animator animator;
+    void Awake() => instance = this;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        StopDancing();
+    }
+
+    public void Dance()
+    {
+        gameObject.SetActive(true);
+        animator.Play("StatueDance", 0, 0);
+    }
+    public void StopDancing()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
