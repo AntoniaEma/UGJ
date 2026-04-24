5319b0a4828188795ff229d86ec4fd3496cc3615
AntoniaEma <antonia.tufanescu@student.upt.ro>
2026-04-24 21:05:34 +0300

    Main Menu initial commit
    Added main menu specific buttons, alongside relevant panels

---
+++ Assets/_Scripts/UI/MainMenu.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainPanel;
+    public GameObject instructionsPanel;
+    public GameObject settingsPanel;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OpenInstructions()
+    {
+        mainPanel.SetActive(false);
+        instructionsPanel.SetActive(true);
+    }
+
+    public void OpenSettings()
+    {
+        mainPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        mainPanel.SetActive(true);
+        instructionsPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Closed"); // util în editor
+    }
+}
