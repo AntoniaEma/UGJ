02e2272c01547516d4aa5bf43ec06c0eec4d3b1f
AntoniaEma <antonia.tufanescu@student.upt.ro>
2026-04-24 22:41:00 +0300

    Main menu 2
    Added adjustable volume with a dummy audio. Added menu background

--- Assets/_Scripts/Audio/AudioManager.cs
+++ Assets/_Scripts/Audio/AudioManager.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
+    public Slider volumeSlider;
+
+    private void Start()
+    {
+        volumeSlider.value = 1f;
+        AudioListener.volume = 1f;
+    }
+
     public void SetVolume(float volume)
     {
         AudioListener.volume = volume;
+        Debug.Log("Volume: " + volume);
     }
 }
