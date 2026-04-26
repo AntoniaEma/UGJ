666f2ac0197480a1004e9890c932f1addbc6d402
dcarpencu <dcarpencu@gmail.com>
2026-04-26 19:34:51 +0300

    ceva

--- Assets/_Scripts/UI/StoryScreen.cs
+++ Assets/_Scripts/UI/StoryScreen.cs
@@ -46,7 +46,6 @@ public class StoryScreen : MonoBehaviour
 
     public void Continue()
     {
-        SoundManager.instance?.StopMenuMusic();
         SceneManager.LoadScene(gameSceneName);
     }
 }
